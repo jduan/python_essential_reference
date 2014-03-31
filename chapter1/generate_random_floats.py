237fff605d9b48fe489911752e6b504599336781
Jingjing Duan <jduan@twitter.com>
2014-03-31 15:50:54 -0700

    make generate_random_floats take a CLI param

--- chapter1/generate_random_floats.py
+++ chapter1/generate_random_floats.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # This script generates a list of random floats and print them to the console.
 
+import sys
 import random
 
-for i in range(10):
+num_of_lines = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+for i in range(num_of_lines):
   print random.random() * 100
