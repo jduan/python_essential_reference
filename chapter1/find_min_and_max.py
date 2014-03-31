92bf74eaa33ffd6a06bc8dd6a162e5667297d420
Jingjing Duan <jduan@twitter.com>
2014-03-31 15:52:16 -0700

    simplify find_min_and_max

--- chapter1/find_min_and_max.py
+++ chapter1/find_min_and_max.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # This script generates a list of random floats and print them to the console.
 
-with open('floats.txt') as fd:
-  lines = fd.readlines()
-
-floats = [float(line) for line in lines]
+floats = [float(line) for line in open('floats.txt')]
 
 print "max is %s" % max(floats)
 print "min is %s" % min(floats)
